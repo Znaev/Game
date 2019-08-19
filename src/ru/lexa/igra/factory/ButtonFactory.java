2311e2ae630efabb1c53358f4e853b7603bc6c27
Misha <znaevm@gmail.com>
2019-08-19 19:31:29 +0300

    Примерный вид

---
+++ src/ru/lexa/igra/factory/ButtonFactory.java
@@ -0,0 +1,11 @@
+package ru.lexa.igra.factory;
+
+import javafx.scene.control.Button;
+
+import java.util.List;
+
+public class ButtonFactory {
+    public static List<Button> generateButtons(){
+        return null;
+    }
+}
---
+++ src/ru/lexa/igra/factory/FlowPaneFactory.java
@@ -0,0 +1,9 @@
+package ru.lexa.igra.factory;
+
+import javafx.scene.layout.FlowPane;
+
+public class FlowPaneFactory {
+    public static FlowPane generateFlowPane(){
+        return null;
+    }
+}
