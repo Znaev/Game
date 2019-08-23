f35d169bbb938fae33f4e70c86b45fb4fbe2810c
Rooney22 <znaevas@mail.ru>
2019-08-23 15:49:36 +0600

    Fixed Main file.

--- src/ru/lexa/igra/Main.java
+++ src/ru/lexa/igra/Main.java
@@ -1,21 +1,14 @@
 package ru.lexa.igra;
 
 import javafx.application.Application;
-import javafx.scene.control.Label;
-import javafx.scene.layout.*;
 import javafx.stage.Stage;
-import ru.lexa.igra.factory.*;
 import ru.lexa.igra.Levels.LevelFirst;
 
 public class Main extends Application {
-    private Label stat_health = new Label("Здоровье:");
-    private Label stat_stamina = new Label("Выносливость:");
-    private Label stat_gold = new Label("Золото:");
-    private Label stat_name = new Label("Имя:");
     @Override
     public void start(final Stage primaryStage) {
         LevelFirst.startLevelFirst(primaryStage);
-}
+    }
 
 
     public static void main(String[] args) {
